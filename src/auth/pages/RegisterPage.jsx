8ad2cefff37c13321732cd62e5b89fe905c57d6e
juanio <juaniorosas.99@gmail.com>
2022-07-22 03:25:00 -0500

    Layouts done

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Google } from '@mui/icons-material'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Usuario"
+              type="user"
+              placeholder="Usuario"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gamil.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contrasena"
+              type="password"
+              placeholder="password"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Confirmar contrasena"
+              type="password"
+              placeholder="password"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={12}>
+              <Button variant='contained' fullWidth>
+                Create
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+               Log in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,13 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <Typography>JournalPage</Typography>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid optio illum reiciendis laboriosam libero praesentium, obcaecati quas dignissimos quae placeat perspiciatis nisi nostrum repellat sit. Aperiam aspernatur facilis inventore sit.
+
+      </Typography>
+    </JournalLayout>
   )
 }
